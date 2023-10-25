9d76b2b34fcc49d78c01def0b26e410526481a44
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-10-25 13:19:39 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,27 @@
 <script>
-    const n = 10, m = 12
-    var lr = Array(n).fill(0)
-    var lc = Array(m).fill(0)
-    var arr = Array(n).fill(0).map(v => Array(m))
+  const n = 10, m = 12
+  var lr = Array(n).fill(0)
+  var lc = Array(m).fill(0)
+  var arr = Array(n).fill(0).map(v => Array(m))
 </script>
 <table>
-    {#each lr as r, i}
-        <tr>
-            {#each lc as _, j}
-                <td on:click={(e)=>{
-                    arr[i][j] ? lr[i]-- : lr[i]++
-                    arr[i][j] ? lc[j]-- : lc[j]++
-                    arr[i][j] = arr[i][j] ? '' : 'h'
-                }} class={arr[i][j]}>{i},{j}</td>
-            {/each}
-            <td class="ro {r?'':'null'}">{r}</td>
-        </tr>
-    {/each}
+  {#each lr as r, i}
     <tr>
-        {#each lc as c, j}
-            <td class="ro {c?'':'null'}">{c}</td>
-        {/each}
+      {#each lc as _, j}
+        <td on:click={(e) => {
+          if (arr[i][j]) {
+            lr[i]--, lc[j]--, arr[i][j] = ''
+          } else {
+            lr[i]++, lc[j]++, arr[i][j] = 'h'
+          }
+        }} class={arr[i][j]}>{i},{j}</td>
+      {/each}
+      <td class="ro {r?'':'null'}">{r}</td>
     </tr>
+  {/each}
+  <tr>
+    {#each lc as c, j}
+      <td class="ro {c?'':'null'}">{c}</td>
+    {/each}
+  </tr>
 </table>
